cb791880e1c76a153054400f0409dbfeac861d70
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-02-05 09:29:15 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Raphael Abano on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
