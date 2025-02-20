43a508fa3fe17571a220b1c4a7d84e1293dffbf4
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-02-20 09:40:36 -0600

    Added website link and enabled directions

--- Za Hunter/LocationDetailsView.swift
+++ Za Hunter/LocationDetailsView.swift
@@ -19,13 +19,33 @@ struct LocationDetailsView: View {
                 .padding()
             Text(mapItem.phoneNumber!)
                 .padding()
+            if let url = mapItem.url {
+                Link(destination: url, label: {
+                    Text("Website")
+                    .padding()
+                })
+            }
+            Button(action: {
+                let latitude = mapItem.placemark.coordinate.latitude
+                let longitude = mapItem.placemark.coordinate.longitude
+                let url = URL(string: "maps://?saddr=&daddr=\(latitude),\(longitude)")
+                if UIApplication.shared.canOpenURL(url!) {
+                    UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+                }
+            }, label: {
+                Text("Get Directions")
+            })
             Spacer()
         }
     }
     // This was created to fix Xcode type checking in "unreasonable" time.
     func getAddress(mapItem : MKMapItem) -> String {
-        let address1 = (mapItem.placemark.subThoroughfare ?? " ") + " " + (mapItem.placemark.thoroughfare ?? " ") + "\n"
-        let address2 = (mapItem.placemark.locality ?? " ") + ", " + (mapItem.placemark.administrativeArea ?? " ") + " "
+        let address1 = (mapItem.placemark.subThoroughfare ?? " ") + " " + (
+            mapItem.placemark.thoroughfare ?? " "
+        ) + "\n"
+        let address2 = (mapItem.placemark.locality ?? " ") + ", " + (
+            mapItem.placemark.administrativeArea ?? " "
+        ) + " "
         return address1 + address2 + (mapItem.placemark.postalCode ?? " ")
     }
 }
